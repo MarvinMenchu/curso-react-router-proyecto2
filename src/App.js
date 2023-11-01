c57602109d0b5ee84bb9301e1c68f94d5cb78c7b
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2023-10-31 21:23:19 -0600

    SE AGREGO ESTADOS DERIVADOS EN APP

--- src/App.js
+++ src/App.js
@@ -9,16 +9,32 @@ const defaultTodos = [
   {text: 'Cortar cebolla', completed: true},
   {text: 'Tomas el curso de Intro a React.js', completed: false},
   {text: 'Llorar con la Llorona', completed: false},
-  {text: 'LALALALALA', completed: false}
+  {text: 'LALALALALA', completed: false},
+  {text: 'Usar estados derivados', completed: true}
 ];
 
 
 function App() {
+
+  const [todos, setTodos] = React.useState(defaultTodos);
+
+  const [searchValue, setSearchValue] = React.useState('');
+
+  // Estados derivados, san variables calculos de un estado
+  // doble negacion !! para saber que estamos trabajando con valores falso y verdadero
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length; 
+
+  console.log("Los usuarios usan Todos de " + searchValue);
+
   return (
     <>
 
-      <TodoCounter completed={16} total={25} />
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList> 
         {defaultTodos.map(todo => (
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,11 +1,9 @@
 import React from "react";
 import "./TodoSearch.css"
 
-function TodoSearch (){
+function TodoSearch ({searchValue, setSearchValue}){
 
-  const [searchValue, setSearchValue] = React.useState('');
-
-  console.log("Los usuarios usan Todos de " + searchValue);
+  
 
     return (
       <input
