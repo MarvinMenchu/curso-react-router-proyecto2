041520ad0295264e327c018a3c148781fb19ecbe
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2023-10-31 20:52:53 -0600

    SE AGREGO ESTADO A TODOSEARCH

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,15 +1,19 @@
+import React from "react";
 import "./TodoSearch.css"
 
 function TodoSearch (){
+
+  const [searchValue, setSearchValue] = React.useState('');
+
+  console.log("Los usuarios usan Todos de " + searchValue);
+
     return (
       <input
         placeholder="Cortar cebolla"
         className="TodoSearch"
+        value={searchValue}
         onChange={(event) => {
-          console.log('Escribiste en el Todo Search');
-          console.log(event);
-          console.log(event.target);
-          console.log(event.target.value);
+          setSearchValue(event.target.value);
         }}
       />
     );
