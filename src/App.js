986997b58d4e51fdc1ecc3ee9dea05e67f8753ce
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2023-11-02 20:53:07 -0600

    SE AGREGO COMPLETADO Y ELIMINADOS TODOs

--- src/App.js
+++ src/App.js
@@ -36,6 +36,24 @@ function App() {
 
   console.log("Los usuarios usan Todos de " + searchValue);
 
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    )
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    )
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
+
   return (
     <>
 
@@ -51,6 +69,8 @@ function App() {
             key={todo.text} 
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
